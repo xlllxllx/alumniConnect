0244821c9a64c1c62489c712e79ed83e874584ce
xlllxllx <xlllxllxwu@gmail.com>
2024-02-13 20:59:06 +0800

    edit of profile

--- alumniConnect/src/app/tabs/tabs.page.ts
+++ alumniConnect/src/app/tabs/tabs.page.ts
@@ -11,10 +11,10 @@ export class TabsPage {
   isStudent = false;
   constructor(private authService: AuthService) {
     this.authService.observeAuthState(user => {
-      if(user && user.email == 'admin@nyp.sg') {
+      if(user && user.email == 'admin1@nyp.sg') {
         this.isAdmin = true;
         this.isStudent = false;
-      } else if (user && user.email != 'admin@nyp.sg'){
+      } else if (user && user.email != 'admin1@nyp.sg'){
         this.isAdmin = false;
         this.isStudent = true;
       } else {
